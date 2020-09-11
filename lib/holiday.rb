f5e603fc658d59998cbf991e347111ca5e6060ed
Eli Huebner <elimhuebner@gmail.com>
2020-09-11 16:46:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,7 +82,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
-      
+      holiday_ar = holiday.split
       puts "  #{holiday.capitalize}: #{supplies.join(", ")}"
     end
   end  
